614733c50612723c84c43b14fa1ff0968249fefd
shubham <shubham.chndrvnshi@gmail.com>
2021-07-25 14:29:36 +0530

    fetch crons completed

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,14 @@ const cluster = require('cluster');
 const axios = require('axios');
 let api1call = false;
 let api2call = false;
-let api3call = false;
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-    await db.set('radis-key', 'redis-value');
-    const mydatapi1calla = await db.get('radis-key');
-    res.render('index', {title: mydata});
+    // await db.set('radis-key', 'redis-value');
+    // const mydatapi1calla = await db.get('radis-key');
+    // res.render('index', {title: mydata});
+    // saveMarketOddsData();
+    res.json("ok");
 });
 
 
@@ -20,13 +21,65 @@ cron.schedule(process.env.CRON_EVENT_LIST, saveEventListData);
 
 cron.schedule(process.env.CRON_MARKET_LIST, saveMarketListData);
 
+cron.schedule(process.env.CRON_ODDS_API, saveMarketOddsData);
+
+function saveMarketOddsData() {
+    console.log("*****************************************************************");
+    console.log("Insert Market ODDS data into DB");
+    console.log("*****************************************************************");
+    if (! api2call) {
+        db.client.hget("event-list", "event", (err, reply) => {
+            if (err) {
+                console.error(err);
+            }
+            if (reply) {
+                let marketidArray = [];
+                reply = JSON.parse(reply);
+                reply.forEach(item => {
+                    item.oddsData.market_id ? marketidArray.push(item.oddsData.market_id) : {};
+                });
+                let config = {
+                    method: 'post',
+                    url: process.env.ODDS_API,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: JSON.stringify(marketidArray)
+                };
+                axios(config).then(function (response) {
+                    response.data.data.items.forEach(item => {
+                        let stringified = stringyfyValues(item);
+                        if (stringified.market_id) {
+                            db.client.hmset(`market-${
+                                stringified.market_id
+                            }`, stringified);
+                        }
+                        console.log("*****************************************************************");
+                        console.log("Insert completed for Market ODDS data");
+                        console.log("*****************************************************************");
+                    })
+                }).catch(function (error) {
+                    console.log(error);
+                });
+            } else {
+                console.log("No event list data");
+            }
+        })
+    }
+}
+
 function saveEventListData() {
+    console.log("*****************************************************************");
     console.log("Insert Event list data into DB");
+    console.log("*****************************************************************");
     axios.get(process.env.EVENT_LIST).then(function (response) {
         api1call = true;
         let result = response.data.data[0];
         result = stringyfyValues(result);
-        db.client.hmset("event-list",result);
+        db.client.hmset("event-list", result);
+        console.log("*****************************************************************");
+        console.log("Insert completed for Event list data");
+        console.log("*****************************************************************");
         api1call = false;
     }).catch(function (error) {
         console.error(error);
@@ -35,50 +88,57 @@ function saveEventListData() {
 
 
 function saveMarketListData() {
+    console.log("*****************************************************************");
     console.log("Insert Market list data into DB");
-    if(!api1call){
-        db.client.hgetall("event-list",(err, reply)=>{
-            if(err){
+    console.log("*****************************************************************");
+    if (! api1call) {
+        db.client.hgetall("event-list", (err, reply) => {
+            if (err) {
                 console.error(err);
             }
-            if(reply){
+            if (reply) {
                 reply = parseValues(reply);
-                reply.event.forEach(item =>{
-                    console.log("Inserting data for event: "+item.eventId);
-                    axios.get(process.env.MARKET_LIST+item.eventId).then(function (response) {
+                reply.event.forEach(item => {
+                    console.log("Inserting data for event: " + item.eventId);
+                    axios.get(process.env.MARKET_LIST + item.eventId).then(function (response) {
                         api2call = true;
-                        if(response.data.data){
+                        if (response.data.data) {
                             let result = stringyfyValues(response.data.data);
-                            db.client.hmset(`event-${item.eventId}`,result);
+                            db.client.hmset(`event-${
+                                item.eventId
+                            }`, result);
                         }
+                        console.log("*****************************************************************");
+                        console.log("Insert completed for Market list data");
+                        console.log("*****************************************************************");
                         api2call = false;
                     }).catch(function (error) {
                         console.error(error);
                     });
                 })
-            }else{
+            } else {
                 console.log("No data in event list");
             }
         });
     }
 }
 
-function stringyfyValues(object){
+function stringyfyValues(object) {
     for (let [key, value] of Object.entries(object)) {
         object[key] = JSON.stringify(value);
-      }
-      return object;
+    }
+    return object;
 }
 
-function parseValues(object){
+function parseValues(object) {
     for (let [key, value] of Object.entries(object)) {
-        try{
+        try {
             object[key] = JSON.parse(value);
-        }catch(e){
+        } catch (e) {
             object[key] = value;
         }
-      }
-      return object;
+    }
+    return object;
 }
 
 module.exports = router;
