ed33b8635d331cd7e3e9c04a1e78413008df19f3
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2021-08-29 14:45:29 +0530

    changes env

--- routes/index.js
+++ routes/index.js
@@ -243,6 +243,7 @@ function saveMarketListData() {
                     let temp = await callMarketListAPI(process.env.MARKET_LIST + item.eventId, item);
                     temp = parseValues(temp);
                     market_list.runners = [];
+                    let keys = ["marketId", "isMarketDataDelayed", "status", "betDelay", "bspReconciled", "complete", "inplay", "numberOfWinners", "numberOfRunners", "numberOfActiveRunners", "lastMatchTime", "totalMatched", "totalAvailable", "crossMatching", "runnersVoidable", "version"];
                     Object.keys(temp).forEach((key)=>{
                         if(Array.isArray(temp[key])){
                             temp[key].forEach((item)=>{
@@ -251,7 +252,7 @@ function saveMarketListData() {
                                 }
                             })
                         }else{
-                            market_list[key] = temp[key];
+                            keys.includes(key) ?  market_list[key] = temp[key] : {} ;
                         }
                     });
                     db.client.hmset("marketList", "marketList", JSON.stringify(market_list));
