0623db2650ad1521760e0f3b40ec5a5e422dc4b1
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2021-08-30 13:10:37 +0530

    final commit

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+require("./routes/Sockets")
 // Defining all the routes
 const index = require('./routes/index');
 const Api = require('./routes/Api');
